81a73865b212824b5c75bbfdaad345a0ffc48919
Uriel Marquez <umarquez.mx@gmail.com>
2025-02-27 23:33:44 -0600

    fix test

--- internal/usecase/crypto_test.go
+++ internal/usecase/crypto_test.go
@@ -1,10 +1,16 @@
 package usecase_test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/umarquez/cryptocoins-go-challenge/internal/domain"
+	"github.com/umarquez/cryptocoins-go-challenge/internal/repository"
+	"github.com/umarquez/cryptocoins-go-challenge/internal/service"
 	"github.com/umarquez/cryptocoins-go-challenge/internal/usecase"
+
+	"github.com/tidwall/buntdb"
 )
 
 func Test_cryptoUseCase_GetCryptos(t *testing.T) {
@@ -21,8 +27,18 @@ func Test_cryptoUseCase_GetCryptos(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			uc := usecase.NewCryptoUseCase()
-			got, err := uc.GetCryptos()
+			db, err := buntdb.Open(":memory:")
+			if err != nil {
+				t.Errorf("GetCryptos() error = %v", err)
+				return
+			}
+			defer db.Close()
+
+			srv := service.GetCryptoService()
+			repo := repository.NewCryptoRepository(db, new(sync.Mutex), time.Minute)
+
+			uc := usecase.NewCryptoUseCase(srv, repo)
+			got, err := uc.GetAllCryptos()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetCryptos() error = %v, wantErr %v", err, tt.wantErr)
 				return
